f70e06448ede1c61bb46d6cbf5aaa2486bb5f1a7
Smirithi <smirithi898@gmail.com>
2023-06-24 14:28:26 +0100

    Add questions 5-8

--- pizza-metrics.sql
+++ pizza-metrics.sql
@@ -22,3 +22,35 @@ SELECT pizza_name, COUNT(order_id) AS Pizzas_ordered
     GROUP BY pizza_name;
     
 -- 6. What was the maximum number of pizzas delivered in a single order?
+SELECT MAX(No_of_pizzas_ordered) AS max_pizzas
+	FROM ( SELECT order_id, COUNT(pizza_id) AS No_of_pizzas_ordered
+			FROM new_customer_orders
+			GROUP BY order_id
+		 ) AS temp_table;
+         
+-- 7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+SELECT customer_id, 
+	SUM(change_in_order = 0) AS pizza_without_change,
+	SUM(change_in_order = 1) AS pizza_with_change
+    FROM 
+		( SELECT c.order_id AS order_id, c.customer_id AS customer_id,
+			CASE
+				WHEN c.exclusion = 0 AND c.extra = 0 THEN 0
+				WHEN c.exclusion > 0 OR c.extra > 0 THEN 1
+			END AS change_in_order
+			FROM runner_orders as r LEFT JOIN new_customer_orders AS c
+			ON r.order_id = c.order_id) AS temp_table
+	GROUP BY customer_id;
+
+-- 8. How many pizzas were delivered that had both exclusions and extras?
+SELECT COUNT(pizza_id) AS pizza_with_exclusions_and_extras
+	FROM (SELECT c.pizza_id AS pizza_id, c.customer_id AS customer_id,
+			CASE
+				WHEN c.exclusion > 0 AND c.extra > 0 THEN 1
+				ELSE 0
+			END AS order_with_changes
+			FROM runner_orders as r LEFT JOIN new_customer_orders AS c
+			ON r.order_id = c.order_id) AS temp_table
+	WHERE order_with_changes = 1;
+    
+-- 9. 
